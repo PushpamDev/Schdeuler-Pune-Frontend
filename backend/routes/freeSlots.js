dec2210374e92fb5299450a7322f00ca4bf312bc
RVM Media <rvmmedia@RVMs-Mac-mini.local>
2025-08-30 18:25:46 +0530

    removed backend

--- backend/routes/freeSlots.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getFreeSlots } = require('../controllers/freeSlotsController');
-
-router.get('/', getFreeSlots);
-
-module.exports = router;
--- backend/routes/skills.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const skillsController = require('../controllers/skillsController');
-
-router.get('/', skillsController.getAllSkills);
-router.post('/', skillsController.createSkill);
-
-module.exports = router;
